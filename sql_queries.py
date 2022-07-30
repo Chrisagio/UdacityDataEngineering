9c9efff735378e1628c5dffc575ac893bc859ef4
Chrisagio <christoph.oellrich@gmail.com>
2022-07-30 18:02:55 +0200

    Added Conflict clause to users insertion

--- sql_queries.py
+++ sql_queries.py
@@ -12,7 +12,7 @@ time_table_drop = "DROP TABLE IF EXISTS time"
 songplay_table_create = ("""CREATE TABLE IF NOT EXISTS songplays
     (
         songplay_id SERIAL PRIMARY KEY,
-        start_time TIME NOT NULL,
+        start_time time NOT NULL,
         user_id INT NOT NULL,
         level TEXT,
         song_id TEXT,
@@ -55,7 +55,7 @@ artist_table_create = ("""CREATE TABLE IF NOT EXISTS artists
 ##Time table with columns: start_time, hour, day, week, month, year, weekday
 time_table_create = ("""CREATE TABLE IF NOT EXISTS time
     (
-        start_time TIME NOT NULL,
+        start_time time NOT NULL,
         hour INT,
         day TEXT,
         week INT,
@@ -88,7 +88,7 @@ user_table_insert = ("""INSERT INTO users
         gender,
         level
     ) VALUES (%s,%s,%s,%s,%s)
-    ON CONFLICT DO NOTHING
+    ON CONFLICT (user_id) DO UPDATE SET user_id = EXCLUDED.user_id
     """)
 
 song_table_insert = ("""INSERT INTO songs
